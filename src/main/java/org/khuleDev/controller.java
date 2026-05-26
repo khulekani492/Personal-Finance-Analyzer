af0fac0048b8738d37e9445f151a65ef094f5587
khulekaniszondo6@gmail.com <khulekaniszondo6@gmail.com>
2026-05-26 23:03:33 +0400

    Javalin server

--- src/main/java/org/khuleDev/controller.java
+++ src/main/java/org/khuleDev/controller.java
@@ -1,4 +1,21 @@
 package org.khuleDev;
 
+import io.javalin.Javalin;
+import io.javalin.rendering.*;
+
+import io.javalin.rendering.*;
+import io.javalin.rendering.template.JavalinThymeleaf;
+
 public class controller {
+    void main() {
+        Javalin.create(config -> {
+
+            config.fileRenderer(new JavalinThymeleaf());
+            config.staticFiles.add("/public");
+            config.routes.get("/",ctx ->{
+                ctx.render("templates/index.html");
+            });
+        }).start(7090);
+    }
+
 }
